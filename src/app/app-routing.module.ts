3aec9053ae08d565e7df686bb9efccf6db7e1681
Rostislav Panchenko <astor.rostislav@gmail.com>
2023-07-21 12:09:54 +0300

    Added two pages (registration and list of participants), navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { ListOfParticipantsComponent } from './list-of-participants/list-of-participants.component';
+
+const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo: "registration_page"},
+  {path: "registration_page", component: RegistrationPageComponent},
+  {path: "list_of_participants", component: ListOfParticipantsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponents = [RegistrationPageComponent, ListOfParticipantsComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { ListOfParticipantsComponent } from './list-of-participants/list-of-participants.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationPageComponent,
+    ListOfParticipantsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
